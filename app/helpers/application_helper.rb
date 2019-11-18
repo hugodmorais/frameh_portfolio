3a196c23475fdee0f81b35c3294de9a4977576b7
hugodmorais <hugodanielmorais@gmail.com>
2019-11-18 21:26:48 +0000

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -61,6 +61,18 @@ module ApplicationHelper
     def active? path
         "active" if current_page? path
     end
+
+    def alerts
+        alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+        if alert
+            alert_generator alert
+        end
+    end
+
+    def alert_generator msg
+        js add_gritter(msg, title: "Hugo Morais", sticky: false)
+    end
 end
 
 
