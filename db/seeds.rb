55bd7328f3b89f49c6fe5ec89d7fb104cf5790b1
hugodmorais <hugodanielmorais@gmail.com>
2020-01-19 11:24:15 +0000

    Seeds fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create!(
+    email: "admin@admin.com",
+    password: "123456",
+    password_confirmation: "123456",
+    name: "Admin User",
+    roles: "site_admin"
+)
 3.times do |topic|
     Topic.create!(
         title: Faker::Movie.quote
